5f9574cbaf6f8f7224cc6e7d112559fd2f607b38
John Varney <john@jdvarney.com>
2021-12-20 12:02:46 -0500

    Added support for messages and SIWA

---
+++ bookE/Community/LoadStates.swift
@@ -0,0 +1,12 @@
+//
+//  LoadStates.swift
+//  bookE
+//
+//  Created by John Varney on 12/18/21.
+//
+
+import Foundation
+
+enum LoadState {
+    case inactive, loading, success, noResults
+}
---
+++ bookE/Community/SharedItem.swift
@@ -0,0 +1,15 @@
+//
+//  SharedItem.swift
+//  bookE
+//
+//  Created by John Varney on 12/18/21.
+//
+
+import Foundation
+
+struct SharedItem: Identifiable {
+    let id: String
+    let title: String
+    let detail: String
+    let completed: Bool
+}
